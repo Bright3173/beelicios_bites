a042e4b1ec52028a57b07e55659ba6d0de583fab
Bright3173 <smartme783@gmail.com>
2025-12-17 11:52:23 +0100

    adding subtitle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 "use client";
 import NewArrivals from "@/components/arrivals/NewArrivals";
-import BannerOne from "@/components/banner/BannerOne";
-import BannerTwo from "@/components/banner/BannerTwo";
+// import BannerOne from "@/components/banner/BannerOne";
+// import BannerTwo from "@/components/banner/BannerTwo";
 import BlogSlider from "@/components/blog/BlogSlider";
-import ExploreCategory from "@/components/category/ExploreCategory";
-import DealSlider from "@/components/deal-slider/DealSlider";
+// import ExploreCategory from "@/components/category/ExploreCategory";
+// import DealSlider from "@/components/deal-slider/DealSlider";
 import HeroSlider from "@/components/hero/HeroSlider";
 import Instagram from "@/components/instagram/Instagram";
 import Services from "@/components/services/Services";
@@ -15,10 +15,10 @@ export default function Home() {
   return (
     <>
       <HeroSlider />
-      <ExploreCategory />
-      <DealSlider />
-      <BannerOne />
-      <BannerTwo />
+      {/* <ExploreCategory /> */}
+      {/* <DealSlider /> */}
+      {/* <BannerOne />
+      <BannerTwo /> */}
       <NewArrivals />
       <Services />
       <TopVendor />
